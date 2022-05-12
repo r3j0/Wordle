c0a0f782db48d40e9ea1afff9a92dc86a9af40d3
r3j0 <readytojoin086@gmail.com>
2022-05-12 15:32:56 +0900

    edit main (database)

--- main.c
+++ main.c
@@ -8,10 +8,17 @@ char wordDatabase[10000][6] = {
 "adman","admen","admin","admit","admix","adobe","adobo","adopt","adore","adorn","adult","aegir","aegis","aeons","aerie","aesop","affix","afire","afoot","afoul","afros","after","again",
 "agape","agate","agave","agent","agile","aging","agism","aglow","agone","agony","agora","agree","ahead","aided","aider","aides","aimed","aimer","aired","aisle","alack","alarm","album",
 "alder","alert","algae","algal","alias","alibi","alice","alien",
-}; // 5/9 100°³
+
+"align","alike","aline","alive","alkyl","allah","allay","alley","allot","allow","alloy","aloes","aloft","aloha","alone","along","aloof","aloud","alpha","altar","alter","amass",
+"amaze","amber","ambit","amble","ameba","ameer","amend","amens","amide","amigo","amine","amino","amiss","amity","amman","among","amour","ample","amply","amuck","amuse","anamy",
+"andes","anele","angel","anger","angle","anglo","angry","angst","anion","anise","ankle","annal","annex","annoy","annul","anode","antic","antsy","anvil","aorta","apace","apart",
+"apery","aphid","apian","aping","apish","apnea","appal","apple","apply","appro","april","apron","apses","apsis","aptly","arabs","araks","arbor","arced","ardor","areal","areas",
+"arena","arent","argon","argot","argue","argus","arias","aries","arise","armed","armor","aroma",
+
+}; // 5/11 200°³
 int workDatabaseSize = 100;
 
 int main(void) {
 	srand(time(NULL));
-	printf("%s", wordDatabase[rand() % 100]);
+	printf("%s", wordDatabase[rand() % 200]);
 }
