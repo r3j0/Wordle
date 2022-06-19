f7329fa80c440a316f3f0d9e6b7ef61da3e45163
r3j0 <readytojoin086@gmail.com>
2022-06-19 22:09:36 +0900

    edit main

--- main.c
+++ main.c
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
-// 총 단어 개수 : 2309개
+// 총 단어 개수 : 2310개
 
 int main(void) {
-	
+	char ch[6];
+	char pt[6];
+	int search = 0;
+	scanf("%s", ch);
+	FILE* fp = fopen("C:\\Users\\박정근\\Desktop\\Code\\Wordle\\all.txt", "r");
+	while (!feof(fp)) {
+		fgets(pt, 6, fp);
+		if (!strcmp(ch, pt)) {
+			search = 1;
+			break;
+		}
+	}
+
+	if (search == 0) {
+		printf("No Search.");
+	}
+	else {
+		printf("Search : %s", pt);
+	}
+
+	return 0;
 }
